757a5dac227e26feb64c7fda0ab341d373c43008
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-27 13:14:15 +0200

    JS courera

---
+++ 07_Coursera js/js/interactivityWithJavascript.js
@@ -0,0 +1,50 @@
+
+
+
+function message(msg) {
+    document.getElementById('output').innerHTML=msg+"event";
+}
+
+function displayDate() {
+    document.getElementById("demo").innerHTML=Date();
+}
+
+
+
+
+
+
+
+
+
+/*
+//Return Values
+function welcomeMsg(msg) {
+    alert(msg);
+    var name = prompt("what is your name?");
+    return name;
+}
+welcomeMsg();
+*/
+/*
+//Functions
+var x="Hello";
+welcomeMsg(x);
+x="Goodbye";
+welcomeMsg(x);
+function welcomeMsg(x) {
+    alert(x);
+}
+welcomeMsg();
+*/
+/*
+var name;
+prompt ("What is your name");
+document.write(name);
+document.write("<h1>"+name+"</h1>");
+document.write(name+7+"<br>");
+document.write(document.title+"<br>");
+var pars=document.getElementsByTagName('p');
+document.write(pars+"<br>");
+document.write(pars.length);
+*/
