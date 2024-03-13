b61fb2809fa147706a642e27f522026b8f0b2ecf
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-03-13 14:35:07 +0100

    new react projekt

--- 10_React/myfirstreactapp/src/App.js
+++ 10_React/myfirstreactapp/src/App.js
@@ -2,6 +2,32 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <table>
+        <tr>
+          <th>A</th>
+          <th>B</th>
+          <th>C</th>
+          <th>D</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>I</td>
+          <td>II</td>
+          <td>III</td>
+          <td>IV</td>
+        </tr>
+      </table>
     </div>
   );
 }
